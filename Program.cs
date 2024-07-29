bab89a3d5996ef4ce5e7071bca89ed4f8195ac59
Eduardo Nathan <143146733+HadawardA7X@users.noreply.github.com>
2024-07-29 00:20:06 -0300

    Adding Project Files

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Calcule a hipotenusa!");
+Console.WriteLine("Digite o lado A: ");
+double a = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Digite o lado B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt (a * a + b * b);
+
+Console.WriteLine("A hipotenusa é: " + c);
+
+Console.ReadKey();
